f5259217561f72bb7210a0ba565fd9f70b72e9b3
Jonathan Quistiano <jhonquistiano@gmail.com>
2026-04-08 15:45:47 -0600

    Ajuste sintaxi

--- src/components/pages/ProveedorPage.tsx
+++ src/components/pages/ProveedorPage.tsx
@@ -875,7 +875,7 @@ export function ProveedorPage() {
                 onClick={() => navigate("/admin")} 
                 className=" cursor-pointer hidden sm:flex items-center gap-1.5 rounded-full border border-green-200 bg-green-50 px-3 py-1 text-[11px] font-semibold text-green-700">
               <span  className="inline-block h-1.5 w-1.5 rounded-full bg-green-500" />
-              Dashboar de administración
+              Portal de administración
             </button>
           
           </div>
